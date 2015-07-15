ec82c9100e5e8f1a29a862b253063d86ebf2af39
Rogier Slag <rogier.slag@gmail.com>
2015-07-15 12:13:25 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+[![inventid logo](https://cdn.inventid.nl/assets/logo-horizontally-ba8ae38ab1f53863fa4e99b977eaa1c7.png)](http://opensource.inventid.nl)
+
+# rfid-reader2keyboard
+
+This is a very simple maven project which reads the code of your rfid chip, and types it in any field.
+
+To run, use `mvn clean install` which will create a jar file which is executable.
+
---
+++ create-release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BUILD_DATE=`date` && \
+   export DATA=`curl -POST -u inventid-deploy:$GITHUB_PASS https://api.github.com/repos/inventid/rfid-reader2keyboard/releases -d "{\"tag_name\": \"v${BUILD_NUMBER}\",\"name\":\"Build of ${BUILD_DATE} (${COMMIT})\"}" -H "Content-Type: application/json" -H "Accept: application/json"` && \
+   export RELEASE_ID=`echo $DATA | jq '.id'` && \
+   export UPLOAD_URL=`echo $DATA | jq '.upload_url' | sed s/\{\?name\}/\?name=rfid-reader2keyboard.jar/g | sed s/\"//g` && \
+   echo $RELEASE_ID && \
+   curl -POST -u inventid-deploy:$GITHUB_PASS $UPLOAD_URL -H "Content-Type: application/java-archive" --data-binary @target/rfid-reader2keyboard-1.0-SNAPSHOT.jar
+
