10d1d18e9536b1b64f69f4ce4d14e44898652414
Rogier Slag <rogier.slag@gmail.com>
2016-05-24 17:01:18 +0200

    Fix Github and Shippable release pushing

--- create-release.sh
+++ create-release.sh
@@ -4,9 +4,12 @@
 set -e
 
 export BUILD_DATE=`date` && \
-   export DATA=`curl -POST -u inventid-deploy:$GITHUB_PASS https://api.github.com/repos/inventid/rfid-reader2keyboard/releases -d "{\"tag_name\": \"v${BUILD_NUMBER}\",\"name\":\"Build of ${BUILD_DATE} (${COMMIT})\"}" -H "Content-Type: application/json" -H "Accept: application/json"` && \
+   export JSON="{\"tag_name\": \"v${BUILD_NUMBER}\",\"name\":\"Build ${BUILD_NUMBER}\",\"body\":\"Build ${BUILD_NUMBER} of ${BUILD_DATE} (${COMMIT})\"}" && \
+   echo $JSON && \
+   export DATA=`curl -s -POST -u inventid-deploy:$GITHUB_PASS https://api.github.com/repos/inventid/rfid-reader2keyboard/releases -d "$JSON" -H "Content-Type: application/json" -H "Accept: application/json"` && \
+   echo $DATA && \
    export RELEASE_ID=`echo $DATA | jq '.id'` && \
    export UPLOAD_URL=`echo $DATA | jq '.upload_url' | sed s/\{\?name,label\}/\?name=rfid-reader2keyboard.jar/g | sed s/\"//g` && \
    echo $RELEASE_ID && \
-   curl -POST -u inventid-deploy:$GITHUB_PASS $UPLOAD_URL -H "Content-Type: application/java-archive" --data-binary @target/rfid-reader2keyboard-1.0-SNAPSHOT-jar-with-dependencies.jar
+   curl -s -POST -u inventid-deploy:$GITHUB_PASS $UPLOAD_URL -H "Content-Type: application/java-archive" --data-binary @target/rfid-reader2keyboard-1.0-SNAPSHOT-jar-with-dependencies.jar
 
